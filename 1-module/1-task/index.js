a0d20a55f934b6339fec73c6fc1d52b3516b1a4c
artTanya <tanya_strelka@mail.ru>
2020-07-31 13:52:07 +0300

    do first task of first lesson

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -4,5 +4,15 @@
  * @returns {number}
  */
 function factorial(n) {
-  // ваш код...
+  let a = 1;
+  let i=1;
+  while (i<=n){
+   a = a*i;
+   i++;
+  }
+  return a;
 }
+factorial(0);
+factorial(1); 
+factorial(3); 
+factorial(5);
